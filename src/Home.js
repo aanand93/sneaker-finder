e29fcd2b9526a6387e5a8103035141b1393b2c5a
aanand93 <alex329@mac.com>
2021-03-15 12:12:37 -0400

    Changed home text

--- src/Home.js
+++ src/Home.js
@@ -6,8 +6,8 @@ const Home = () => {
 			<h3>
 				Welcome to my Sneaker Finder. Click the Sneaker Gallery link above to
 				view all the sneakers we have. If you want to see more details about any
-				shoe feel free to click on shoe and you will be displayed the info. If
-				you want to view a specific style of shoe you can click through the
+				shoe feel free to click on the shoe and you will be displayed the info.
+				If you want to view a specific style of shoe you can click through the
 				links above todo so.
 			</h3>
 			<h4>
